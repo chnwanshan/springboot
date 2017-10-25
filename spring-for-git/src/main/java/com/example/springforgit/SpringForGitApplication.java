f36186d18c43fea32f7f9268a5453531411a660d
chnwanshan <32873741+chnwanshan@users.noreply.github.com>
2017-10-26 00:58:18 +0800

    Add files via upload

---
+++ spring-for-git/src/main/java/com/example/springforgit/SpringForGitApplication.java
@@ -0,0 +1,12 @@
+package com.example.springforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForGitApplication.class, args);
+	}
+}
